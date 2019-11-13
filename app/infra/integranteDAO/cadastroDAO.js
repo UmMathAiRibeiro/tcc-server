4baa3211edd7d1d0e79ee105f4011e1bbf91ae0c
UmMathAiRibeiro <ummathairibeiro@gmail.com>
2019-11-13 12:13:52 -0300

    13112019 12:13

--- app/infra/integranteDAO/cadastroDAO.js
+++ app/infra/integranteDAO/cadastroDAO.js
@@ -2,8 +2,8 @@ function cadastroDAO(conection) {
     this._conection = conection;
 }
 cadastroDAO.prototype.cadastroIntegrante = function (data, callback) {
-    this._conection.query('INSERT INTO integrante VALUES(DEFAULT,?,?,?,?,?,?,?,?)',
-        [data.iduser, data.nome, data.data_nasc, data.peso, data.altura, data.imc, data.classificacao, data.objetivo], callback);
+    this._conection.query('INSERT INTO integrante VALUES(DEFAULT,?,?,?,?,?,?,?,?,?)',
+        [data.iduser, data.nome, data.data_nasc, data.peso, data.altura, data.imc, data.classificacao, data.objetivo, data.sexo], callback);
 };
 module.exports = function () {
     return cadastroDAO;
